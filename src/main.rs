a9284acad449bdb78d16d9b86e166b7ed00211dd
Marcin Karczewski <marcin.karczewski@mobica.com>
2024-10-06 12:35:51 +0200

    algorithm implementation added

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use embassy_sync::channel::Channel;
 use embassy_time::{Delay, Duration};
 use heapless::String;
 use {defmt_rtt as _, panic_probe as _};
+use fastrand;
+use libm::sinf;
 
 static SIGNAL_CHANNEL: Channel<ThreadModeRawMutex, SignalType, 4> = Channel::new();
 static PUBLISH_CHANNEL: Channel<ThreadModeRawMutex, PublishSignalType, 4> = Channel::new();
@@ -58,22 +60,61 @@ async fn main(spawner: Spawner) {
 
 #[embassy_executor::task]
 async fn sine_generator(seed: u16) {
-    todo!();
+   let mut rnd = fastrand::Rng::with_seed(seed.into());
+   let mut degree = 0.0;
+   loop
+   {
+      let noise = rnd.f32();
+      let sin_val = sinf(degree + noise);
+
+      SIGNAL_CHANNEL.send(SignalType::Sine(sin_val)).await;
+      embassy_time::Timer::after(Duration::from_millis(150)).await;
+
+      degree = degree + 0.0872665; //increment by 5 degrees
+   }
 }
 
 #[embassy_executor::task]
 async fn square_generator(seed: u16) {
-    todo!();
+   let mut rnd = fastrand::Rng::with_seed(seed.into());
+   let mut counter: u32 = 0;
+
+   let square_high: f32 = 20.0;
+   let square_low: f32 = -20.0;
+   loop
+   {
+      let noise = rnd.f32();
+
+      let mut square_val: f32 = square_high + noise;
+
+      if counter > 20 {
+         square_val = square_low + noise;
+      }
+
+      SIGNAL_CHANNEL.send(SignalType::Sine(square_val)).await;
+
+      embassy_time::Timer::after(Duration::from_millis(50)).await;
+
+      counter = counter +1;
+
+      if counter >= 40{
+         counter = 0;
+      }
+   }
 }
 
 #[embassy_executor::task]
 async fn filter_data() {
-    todo!();
+   loop {
+       //TODO receive SIGNAL_CHANNEL and publish on PUBLIC_CHANNEL
+   }
 }
 
 #[embassy_executor::task]
 async fn send_to_pc(
     mut uart: Uart<'static, peripherals::USART3, peripherals::DMA1_CH3, peripherals::DMA1_CH1>,
 ) {
-    todo!();
+loop {
+    //TODO GET from SIGNAL_CHANNEL and write to UARTl
+}
 }
