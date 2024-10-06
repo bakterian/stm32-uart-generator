0aaabf67c10d1c66e045e883459059ae543157b1
Marcin Karczewski <marcin.karczewski@mobica.com>
2024-10-06 16:00:00 +0200

    created UART test prog

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,29 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-use core::num::Wrapping;
-
 use defmt::{info, unwrap};
 use embassy_executor::Spawner;
-use embassy_stm32::adc::Adc;
 use embassy_stm32::usart::{Config, Uart};
 use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::channel::Channel;
-use embassy_time::{Delay, Duration};
+use embassy_time::Duration;
 use heapless::String;
 use {defmt_rtt as _, panic_probe as _};
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_stm32::exti::ExtiInput;
 
-static SIGNAL_CHANNEL: Channel<ThreadModeRawMutex, SignalType, 4> = Channel::new();
-static PUBLISH_CHANNEL: Channel<ThreadModeRawMutex, PublishSignalType, 4> = Channel::new();
+static PUBLISH_CHANNEL: Channel<ThreadModeRawMutex,String<64>, 4> = Channel::new();
 
 bind_interrupts!(struct Irqs {
     USART3 => usart::InterruptHandler<peripherals::USART3>;
 });
 
-enum SignalType {
-    Sine(f32),
-    Square(f32),
-}
-
-enum PublishSignalType {
-    Sine(f32, f32),
-    Square(f32, f32),
-}
-
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    info!("Hello World!");
-
     let p = embassy_stm32::init(Default::default());
+
+    info!("Prog start!");
 
     let config = Config::default();
     let usart = Uart::new(
@@ -44,99 +31,46 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
-    let mut delay = Delay;
-    let mut adc = Adc::new(p.ADC1, &mut delay);
-    let mut pin = p.PC4;
+    let button = Input::new(p.PC13, Pull::Up);
+    let button = ExtiInput::new(button, p.EXTI13);
 
-    let seed = adc.read(&mut pin);
-    unwrap!(spawner.spawn(sine_generator(seed)));
-    let seed = adc.read(&mut pin);
-    unwrap!(spawner.spawn(square_generator(seed)));
-    unwrap!(spawner.spawn(filter_data()));
-    unwrap!(spawner.spawn(send_to_pc(usart)));
+    let led = Output::new(p.PA5, Level::High, Speed::Low);
+
+    unwrap!(spawner.spawn(check_user_button(button)));
+    unwrap!(spawner.spawn(send_to_pc(usart, led)));
 }
 
 #[embassy_executor::task]
-async fn sine_generator(seed: u16) {
-    let mut rnd = fastrand::Rng::with_seed(seed.into());
-    let mut current = 0.0;
+async fn check_user_button(mut button: ExtiInput<'static, peripherals::PC13>) {
     loop {
-        let noise = (rnd.f32() - 0.5) * 0.2;
-        SIGNAL_CHANNEL
-            .send(SignalType::Sine(libm::sinf(current) + noise))
-            .await;
-        embassy_time::Timer::after(Duration::from_millis(150)).await;
-        current += 0.01;
-    }
+            //0. waiting for someone to press the user button and cause a falling voltage edge
+            button.wait_for_falling_edge().await;
+
+            //1. place some smart the to the buffer
+            let buf = String::<64>::try_from("User Button press identfied")
+               .expect("problem creating heapless string from slice");
+
+            //2. added the information to our publish queue
+            //info!("new UART data to be sent!");
+            PUBLISH_CHANNEL.send(buf).await
+         }
 }
 
 #[embassy_executor::task]
-async fn square_generator(seed: u16) {
-    let mut rnd = fastrand::Rng::with_seed(seed.into());
-    let mut current = Wrapping(0);
-    loop {
-        let noise = (rnd.f32() - 0.5) * 0.2;
-        if (current.0 / 50) % 2 == 0 {
-            SIGNAL_CHANNEL.send(SignalType::Square(1.0 + noise)).await;
-        } else {
-            SIGNAL_CHANNEL.send(SignalType::Square(0.0 + noise)).await;
-        }
-        embassy_time::Timer::after(Duration::from_millis(50)).await;
-        current += 1;
-    }
-}
+async fn send_to_pc(mut uart: Uart<'static, peripherals::USART3, peripherals::DMA1_CH3, peripherals::DMA1_CH1>,
+                    mut led: Output<'static, peripherals::PA5>) {
+    loop
+     {
+        // 0. waiting for new items in the publish queue
+        let str_to_publish = PUBLISH_CHANNEL.receive().await;
 
-struct Filter {
-    value: f32,
-}
+        // 1. Send-out the provided string data
+        uart.write(str_to_publish.as_bytes()).await.unwrap();
 
-impl Filter {
-    fn new() -> Self {
-        Self { value: 0.0 }
-    }
-
-    pub fn filter(&mut self, value: f32) -> f32 {
-        let alpha = 0.7;
-        self.value = self.value * alpha + (1.0 - alpha) * value;
-
-        self.value
-    }
-}
-
-#[embassy_executor::task]
-async fn filter_data() {
-    let mut sine_filter = Filter::new();
-    let mut square_filter = Filter::new();
-    loop {
-        match SIGNAL_CHANNEL.receive().await {
-            SignalType::Sine(v) => {
-                PUBLISH_CHANNEL
-                    .send(PublishSignalType::Sine(v, sine_filter.filter(v)))
-                    .await
-            }
-            SignalType::Square(v) => {
-                PUBLISH_CHANNEL
-                    .send(PublishSignalType::Square(v, square_filter.filter(v)))
-                    .await
-            }
-        };
-    }
-}
-
-#[embassy_executor::task]
-async fn send_to_pc(mut uart: Uart<'static, peripherals::USART3, peripherals::DMA1_CH3, peripherals::DMA1_CH1>) {
-    loop {
-        let d = PUBLISH_CHANNEL.receive().await;
-        let mut buf = String::<64>::new();
-        match d {
-            PublishSignalType::Sine(raw, filtered) => {
-                core::write!(&mut buf, "SINE,{},{}\r\n", raw, filtered).unwrap();
-                uart.write(buf.as_bytes()).await.unwrap();
-            }
-            PublishSignalType::Square(raw, filtered) => {
-                core::write!(&mut buf, "SQUARE,{},{}\r\n", raw, filtered).unwrap();
-                uart.write(buf.as_bytes()).await.unwrap();
-            }
-        };
+        // 2. blink wiht the LED
+        led.set_high();
+        embassy_time::Timer::after(Duration::from_secs(1)).await;
+        led.set_low();
+        embassy_time::Timer::after(Duration::from_secs(1)).await;
     }
 }
