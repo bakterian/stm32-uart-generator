f481eb2596320316d4979ae1071d39599fb2f733
Marcin Karczewski <marcin.karczewski@mobica.com>
2024-10-08 10:25:45 +0200

    possible to flash after embassy-executor feature removal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::Duration;
 use {defmt_rtt as _, panic_probe as _};
@@ -11,13 +12,16 @@ use embassy_stm32::gpio::{Level, Output, Speed};
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
+    info!("prog start");
+
     let mut led = Output::new(p.PA5, Level::High, Speed::Low);
 
     loop
      {
         led.set_high();
-        embassy_time::Timer::after(Duration::from_secs(1)).await;
+        embassy_time::Timer::after(Duration::from_secs(3)).await;
         led.set_low();
-        embassy_time::Timer::after(Duration::from_secs(1)).await;
+        embassy_time::Timer::after(Duration::from_secs(3)).await;
+        info!("6 sec tick");
     }
 }
